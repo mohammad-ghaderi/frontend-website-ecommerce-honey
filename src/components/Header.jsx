d9279b31f3d72d6f5d06dfaee4e1ad2e0e42f464
mohammadGhaderi <mohammaduniqe2002@gmail.com>
2023-07-17 02:17:14 +0330

    project started and the base of the header,footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav, Container, Row, Image} from 'react-bootstrap';
+import logo from '../images/logo.png'
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar expand="lg" variant='light' bg='light' className='border-bottom border-secondary p-0'>
+            <Container className=''>
+                <Navbar.Brand href="" className='fs-2 m-0'>
+                    <Image src={logo} sizes='100%' height={50} />
+                </Navbar.Brand>
+                <Nav className="me-auto d-flex flex-row fs-5 py-2">
+                    <Nav.Link className='px-2 '><i className='fas fa-user fs-2'></i></Nav.Link>
+                    <Nav.Link className='px-2 '><i className='fas fa-shopping-cart fs-2'></i></Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
